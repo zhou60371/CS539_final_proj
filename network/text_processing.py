fa43b0905cad35e466b8102814adf5227c7e0355
Loafer <loaferzk@LoaferdeMBP.lan>
2021-12-12 16:17:07 -0500

    text embedding obtain

--- network/text_processing.py
+++ network/text_processing.py
@@ -1,1 +1,20 @@
 import torch
+import transformers
+from transformers import BertModel, BertTokenizer
+
+class text_processing():
+    def __init__(self, text):
+        self.model = BertModel.from_pretrained("bert-base-uncase")
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncase")
+        self.text = text
+
+    # Apply Bert to obtain embedding
+    # pending for pad, mask usage
+    def text_encoder(self, text):
+        ids = torch.tensor([self.tokenizer(text, add_special_tokens=True)])
+        embedding = self.model(ids)
+        return embedding
+
+    def grab_position(self, text, entity):
+        pass
+
