bb68ace9d5278eedc8cf387d3977e80d0f812c90
Yiqin Zhao <yiqinzhao@outlook.com>
2021-12-13 22:10:25 -0500

    :twisted_rightwards_arrows: Fixing conflicts

--- model/network.py
+++ model/network.py
@@ -9,9 +9,6 @@ from model.utils import create_img_encoder
 from model.utils import euclidean_distance
 from model.utils import eucl_dist_output_shape
 
-from network.text_processing import text_processing
-
-
 resnet = ResNet50(include_top=True, weights='imagenet')
 
 for layer in resnet.layers:
@@ -22,15 +19,6 @@ input_img = Input(shape=(224, 224, 3))
 
 encoded_txt = create_txt_encoder()(input_txt)
 encoded_img = create_img_encoder(resnet)(input_img)
-
-<<<<<<< HEAD
-# txt_enc = text_processing()
-# encoded_txt = txt_enc.text_encoder(input_txt)
-
-encoded_txt = txt_enc(input_txt)
-encoded_img = img_enc(input_img)
-=======
->>>>>>> 05234b15729f5d34f37936bade87334fdf932d87
 
 distance = Lambda(euclidean_distance,
                   output_shape=eucl_dist_output_shape)([encoded_txt, encoded_img])
